15c4e29b0fce4f77245743f35525d1161393d932
bowly01 <putita.pai@ku.th>
2022-11-21 16:04:08 +0700

    first-menu

---
+++ src/views/menu.vue
@@ -0,0 +1,21 @@
+<template>
+    <link rel="stylesheet" href="../src/style.css">
+    <div class="header">
+        <h1>my menu</h1>
+    </div>
+        
+    <div class="bg-regular">
+        <p>chack</p>
+    </div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
