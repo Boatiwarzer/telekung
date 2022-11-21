8c4fa4c865a31d36cbf090b826bc7c18dc0f3b06
Earth_Hataichanok <98453083+HataichanokKladngam@users.noreply.github.com>
2022-11-21 16:06:06 +0700

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,30 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-
-</script>
-
 <template>
-  <header>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">คอร์ส</RouterLink>
-      </nav>
-  </header>
+  <component :is="layout">
+    <div></div>
+    <RouterView />
+  </component>
 
-  <RouterView />
-  <div>
-    telekung 
-  </div>
 </template>
 
 
+<script>
+import Main from '@/layouts/Main.vue'
+import Menu from '@/layouts/Menu.vue'
+import Select from '@/layouts/Select.vue'
+import Default from '@/layouts/Default.vue'
+export default {
+  components:
+  {
+    Main,
+    Menu,
+    Select,
+    Default
+  },
+  computed:{
+    layout(){
+      console.log(this.$route.meta);
+      return this.$route.meta.layout || Default;
+    },
+  },
+};
+</script>
