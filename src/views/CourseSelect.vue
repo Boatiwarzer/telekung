8d08b997bb66a9d8e9692cfdc8c0d6f2e961d492
Earth_Hataichanok <98453083+HataichanokKladngam@users.noreply.github.com>
2022-11-21 20:22:03 +0700

    commit e

--- src/views/CourseSelect.vue
+++ src/views/CourseSelect.vue
@@ -1,6 +1,12 @@
 
 <template>
     <link rel="stylesheet" href="../assets/main.css">
+    <div class="">
+        <router-link to="/" class="button">Home</router-link>
+        <router-link to="/menu" class="button" >Menu</router-link>
+        <router-link to="/select" class="button" >Table</router-link>
+        
+    </div>
     <div class="box">
         <form>
         <label for="table">Select table:</label>
