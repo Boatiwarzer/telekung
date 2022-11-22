2946bad021ab485a7b1c9c8d9a49fc1a8540906a
Earth_Hataichanok <98453083+HataichanokKladngam@users.noreply.github.com>
2022-11-22 14:50:02 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -16,13 +16,15 @@ import { RouterLink, RouterView } from 'vue-router'
 import Main from '@/layouts/Main.vue'
 import Menu from '@/views/menu.vue'
 import Select from '@/views/CourseSelect.vue'
+import Order from '@/views/Order.vue'
 // import Default from '@/layouts/Default.vue'
 export default {
   components:
   {
     Main,
     Menu,
-    Select
+    Select,
+    Order
     // Select,
     // Default
   },
